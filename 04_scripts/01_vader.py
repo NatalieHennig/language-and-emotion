d210ab27a59508b7332329ff0fe0386515e0d914
NatalieHennig <natalie-tieda.hennig@uzh.ch>
2023-11-20 14:51:13 +0100

    add vader script to markdown

---
+++ 04_scripts/01_vader.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+
+#nltk.download('vader_lexicon')
+
+def assign_polarity_score(nr, pos=0.02, neg=-0.02):
+  df = pd.read_csv("/Users/nataliehennig/Documents/language-and-emotion/05_output/{}_text_tokens.csv".format(nr))
+  
+  # get polarity scores from VADER
+  sentiments = SentimentIntensityAnalyzer()
+  df['polarity_score'] = [sentiments.polarity_scores(i)["compound"] for i in df["token"]]
+  
+  # assign categorical values to the polarity scores
+  score = df["polarity_score"].values
+  sentiment = []
+  
+  for i in score:
+      if i >= pos:
+          sentiment.append('Positive')
+      elif i <= neg:
+          sentiment.append('Negative')
+      else:
+          sentiment.append('Neutral')
+          
+  df["sentiment"] = sentiment
+  
+  #df.drop(df.columns[[4]], axis=1, inplace=True)
+  df.to_csv("/Users/nataliehennig/Documents/language-and-emotion/05_output/{}_vader_scores.csv".format(nr))
